bf17a8d30ecf569ad4c978431721bd90f73c3704
sugiartofahmi <sugiartofahmi@gmail.com>
2022-12-03 04:07:28 +0700

    Fahmi Sugiarto | Ouput Json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer-core";
-const result = [];
+import fs from "fs";
+const result = { result: [] };
 const app = puppeteer;
 const browser = await app.launch({
   headless: false,
@@ -18,13 +19,22 @@ const ProductName = await page.evaluate(() =>
     (e) => e.innerHTML
   )
 );
-for (let i = 1; i <= ProductName.length; i++) {
+for (let i = 1; i < ProductName.length; i++) {
   const ranking = await page.$x(`//*[@id="w0"]/table/tbody/tr[${i}]/td[1]`);
   const name = await page.$x(`//*[@id="w0"]/table/tbody/tr[${i}]/td[4]`);
   let getRanking = await page.evaluate((el) => el.textContent, ranking[0]);
   let getName = await page.evaluate((el) => el.textContent, name[0]);
   let obj = { ranking: getRanking, name: getName.replace(" More..", "") };
 
-  result.push(obj);
+  result.result.push(obj);
 }
 console.log(result);
+
+fs.writeFile("output.json", JSON.stringify(result), "utf8", function (err) {
+  if (err) {
+    console.log("An error occured while writing JSON Object to File.");
+    return console.log(err);
+  }
+
+  console.log("JSON file has been saved.");
+});
